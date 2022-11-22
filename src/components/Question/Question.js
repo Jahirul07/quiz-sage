1dc52b0df30867dd3171f93dffa28243268b70de
Jahirul Islam <jahirul@elyranur.com>
2022-11-22 22:09:05 +0600

    eyeClick add

--- src/components/Question/Question.js
+++ src/components/Question/Question.js
@@ -1,27 +1,42 @@
 import React from 'react';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { AiFillEye } from 'react-icons/ai';
 
 const Question = ({quiz}) => {
-    const {question, options,} = quiz;
-    console.log(options)
+    const {question, options, correctAnswer} = quiz;
+    const eyeClick = () =>{
+        toast.success(`${correctAnswer}`, {
+            position: toast.POSITION.TOP_CENTER
+        });
+    }
+    const handleRightClick = value =>{
+        if(value === correctAnswer){
+            toast.success('Right Answer !', {
+                position: toast.POSITION.TOP_CENTER
+            });
+        }
+        else{
+            toast.error('Wrong Answer !', {
+                position: toast.POSITION.TOP_CENTER
+            });
+        }
+    }
     return (
-        <div className='flex'>
+            <div className='flex justify-center'>
             <div className='border-2 w-4/5 my-4 mx-5 p-10 rounded-lg shadow-md'>
-            <h1 className='text-xl font-medium'>Question: {question}</h1>
-            <form className='font-normal' action="">
-                
-                <label ><input type="radio" name="radio" id="" />{options[0]}</label><br />
-                
-                <label ><input type="radio" name="radio" id="" />{options[1]}</label><br />
-                
-                <label ><input type="radio" name="radio" id="" />{options[2]}</label><br />
-                
-                <label ><input type="radio" name="radio" id="" />{options[3]}</label><br />
-            </form>
+            <div className='flex justify-between'>
+            <p className='text-xl font-medium'>Question: {question}</p>
+            <p onClick={eyeClick} className='text-2xl'><AiFillEye/></p>
             </div>
-            <div className='w-1/5'>
-
+            <div className='font-normal m-5'>
+                {
+                    options.map((option, index) => <li className='cursor-pointer' onClick={() => handleRightClick(option)} key={index}>{option}</li>)
+                }
+                <ToastContainer></ToastContainer>
             </div>
-        </div>
+            </div>
+            </div>
     );
 };
 
